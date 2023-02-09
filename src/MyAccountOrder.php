901e69eb82771d245b57baa13d5ce6804e4ac454
Muhammad Awais <awais300@gmail.com>
2023-02-09 20:03:51 +0500

    add new column on order table under my account

---
+++ src/MyAccountOrder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace EWA\RCTool;
+
+use EWA\RCTool\Admin\Order;
+
+defined('ABSPATH') || exit;
+
+/**
+ * Class Order
+ * @package EWA\RCTool
+ */
+
+class MyAccountOrder
+{
+	/**
+	 * Construct the plugin.
+	 */
+	public function __construct()
+	{
+		add_filter('woocommerce_account_orders_columns', array($this, 'add_account_orders_column'), 10, 1);
+		add_action('woocommerce_my_account_my_orders_column_' . Order::ORDER_EXPECTED_SHIPPING_DATE_FIELD, array($this, 'display_column_details'));
+	}
+
+	/**
+	 * Add a new column header.
+	 *
+	 * @var array $columns
+	 * @return array
+	 *
+	 **/
+	public function add_account_orders_column($columns)
+	{
+		$columns[Order::ORDER_EXPECTED_SHIPPING_DATE_FIELD] = __('Expected Shipping Date', 'rct-customization');
+		return $columns;
+	}
+
+	/**
+	 * Add a new column header.
+	 *
+	 * @var wc_order $order
+	 *
+	 **/
+	public function display_column_details($order)
+	{
+		if ($value = $order->get_meta(Order::ORDER_EXPECTED_SHIPPING_DATE_FIELD)) {
+			echo esc_html($value);
+		}
+	}
+}
