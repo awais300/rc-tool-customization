65633ba3c848d78433b5b0b9f7fc07649a4b5832
Muhammad Awais <awais300@gmail.com>
2023-02-19 02:08:25 +0500

    add template loader class

---
+++ src/TemplateLoader.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace EWA\RCTool;
+
+defined('ABSPATH') || exit;
+
+/**
+ * Class TemplateLoader
+ * @package EWA\RCTool
+ */
+
+class TemplateLoader extends Singleton
+{
+
+	/**
+	 * Loads a template.
+	 * @param  string $template_name
+	 * @param  array $args
+	 * @param  string $template_path
+	 * @param  bool $echo
+	 *
+	 */
+	public function get_template($template_name, $args = array(), $template_path, $echo = false)
+	{
+		$output = null;
+
+		$template_path = $template_path . $template_name;
+
+		if (file_exists($template_path)) {
+			extract($args); // @codingStandardsIgnoreLine required for template.
+
+			ob_start();
+			include $template_path;
+			$output = ob_get_clean();
+		} else {
+			throw new \Exception(__('Specified path does not exist', 'degf-customization'));
+		}
+
+		if ($echo) {
+			print $output;
+		} else {
+			return $output;
+		}
+	}
+}
