4c31edce22b39b4f00b392ad192b4ecb99618d9b
Muhammad Awais <awais300@gmail.com>
2023-02-25 19:34:48 +0500

    hide cart section

--- assets/js/rct-customization-frontend.js
+++ assets/js/rct-customization-frontend.js
@@ -1,2 +1,7 @@
 jQuery(document).ready(function($) {
+    $(document).on("gform_confirmation_loaded", function(e, form_id) {
+        if(form_id == 3) {
+            $('.woocommerce-notices-wrapper, .woocommerce-cart-form').hide();
+        }
+    });
 });
