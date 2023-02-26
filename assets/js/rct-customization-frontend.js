35bbc46231575792f543fa3e67ef63ddd04c5416
Muhammad Awais <awais300@gmail.com>
2023-02-27 01:17:35 +0500

    use object dynamically

--- assets/js/rct-customization-frontend.js
+++ assets/js/rct-customization-frontend.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function($) {
+    console.log(RCT_OBJ.form_id);
     $(document).on("gform_confirmation_loaded", function(e, form_id) {
-        if(form_id == 3) {
+        console.log(RCT_OBJ.form_id);
+        if (form_id == RCT_OBJ.form_id) {
             $('.woocommerce-notices-wrapper, .woocommerce-cart-form').hide();
         }
     });
