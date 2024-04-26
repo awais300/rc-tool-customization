1875ba5eb3b090c1a36277c7131c84cc5f2f946d
Muhammad Awais <awais300@gmail.com>
2024-04-27 04:12:34 +0500

    clean up & dockblock addition

--- src/MyAccountOrder.php
+++ src/MyAccountOrder.php
@@ -7,44 +7,44 @@ use EWA\RCTool\Admin\Order;
 defined('ABSPATH') || exit;
 
 /**
- * Class Order
+ * Class MyAccountOrder
+ * Handles customization of the My Account Orders page.
+ *
  * @package EWA\RCTool
  */
-
 class MyAccountOrder
 {
-	/**
-	 * Construct the plugin.
-	 */
-	public function __construct()
-	{
-		add_filter('woocommerce_account_orders_columns', array($this, 'add_account_orders_column'), 10, 1);
-		add_action('woocommerce_my_account_my_orders_column_' . Order::ORDER_EXPECTED_SHIPPING_DATE_FIELD, array($this, 'display_column_details'));
-	}
+    /**
+     * Construct the MyAccountOrder class.
+     */
+    public function __construct()
+    {
+        add_filter('woocommerce_account_orders_columns', array($this, 'add_account_orders_column'), 10, 1);
+        add_action('woocommerce_my_account_my_orders_column_' . Order::ORDER_EXPECTED_SHIPPING_DATE_FIELD, array($this, 'display_column_details'));
+    }
 
-	/**
-	 * Add a new column header.
-	 *
-	 * @var array $columns
-	 * @return array
-	 *
-	 **/
-	public function add_account_orders_column($columns)
-	{
-		$columns[Order::ORDER_EXPECTED_SHIPPING_DATE_FIELD] = __('Expected Shipping Date', 'rct-customization');
-		return $columns;
-	}
+    /**
+     * Add a new column header.
+     *
+     * @param array $columns The existing columns.
+     * @return array The modified columns.
+     */
+    public function add_account_orders_column($columns)
+    {
+        $columns[Order::ORDER_EXPECTED_SHIPPING_DATE_FIELD] = __('Expected Shipping Date', 'rct-customization');
+        return $columns;
+    }
 
-	/**
-	 * Add a new column header.
-	 *
-	 * @var wc_order $order
-	 *
-	 **/
-	public function display_column_details($order)
-	{
-		if ($value = $order->get_meta(Order::ORDER_EXPECTED_SHIPPING_DATE_FIELD)) {
-			echo esc_html($value);
-		}
-	}
+    /**
+     * Display the details for the custom column.
+     *
+     * @param \WC_Order $order The order object.
+     * @return void
+     */
+    public function display_column_details($order)
+    {
+        if ($value = $order->get_meta(Order::ORDER_EXPECTED_SHIPPING_DATE_FIELD)) {
+            echo esc_html($value);
+        }
+    }
 }
