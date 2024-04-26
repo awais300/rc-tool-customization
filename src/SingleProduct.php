89c0ab88127b12516c56b3c049164a1c572dc834
Muhammad Awais <awais300@gmail.com>
2024-04-27 04:13:40 +0500

    cleanup & docblock addition

--- src/SingleProduct.php
+++ src/SingleProduct.php
@@ -4,82 +4,72 @@ namespace EWA\RCTool;
 
 use EWA\RCTool\Admin\Product\LeadTime;
 use EWA\RCTool\Admin\Settings;
+use WC_Product;
 
 defined('ABSPATH') || exit;
 
 /**
  * Class SingleProduct
+ * Handles single product display customization.
+ *
  * @package EWA\RCTool
  */
-
 class SingleProduct
 {
-	/**
-	 * Construct the plugin.
-	 */
-	public function __construct()
-	{
-		add_action('woocommerce_before_add_to_cart_form', array($this, 'display_lead_time_message'));
-		add_filter('woocommerce_product_single_add_to_cart_text', array($this, 'custom_add_to_cart_text'), 10, 2);
-		//add_filter('woocommerce_product_add_to_cart_text', array($this, 'custom_add_to_cart_text'), 10, 2);
+    /**
+     * Construct the class.
+     */
+    public function __construct()
+    {
+        add_action('woocommerce_before_add_to_cart_form', array($this, 'display_lead_time_message'));
+        add_filter('woocommerce_product_single_add_to_cart_text', array($this, 'custom_add_to_cart_text'), 10, 2);
+    }
 
-		//add_action('init', array($this, 'action_init'));
-	}
+    /**
+     * Display the lead time message.
+     *
+     * @return void
+     * @throws \Exception If the product is not a WooCommerce product.
+     */
+    public function display_lead_time_message()
+    {
+        global $post;
+        if (!is_product() || empty($post)) {
+            return;
+        }
 
-	public function action_init() {
-		if(is_admin()) {
-			return;
-		}
+        $product = wc_get_product(get_the_ID());
+        if (!is_a($product, 'WC_Product')) {
+            throw new \Exception(__('Not a WooCommerce product', 'rct-customization'));
+        }
 
-		dd($_POST);
-		$data = wp_unslash($_POST['pc_configurator_data']);
+        $lead_time_message = !empty($product->get_meta(LeadTime::PRODUCT_LEAD_TIME_FIELD)) ? $product->get_meta(LeadTime::PRODUCT_LEAD_TIME_FIELD) : get_option(Settings::GLOBAL_LEAD_TIME_FIELD);
 
-		dd($data);
-		$arr = json_decode($data, true);
+        if (!empty($lead_time_message)) {
+            echo '<div class="rct-lead-time-message rct-woocommerce-store-notice">';
+            echo '<h5><i class="fa fa-truck" aria-hidden="true"></i> Lead Time</h5>';
+            echo '<p>' . $lead_time_message . '</p>';
+            echo '</div>';
+        }
+    }
 
-		dd($arr);
-	}
+    /**
+     * Add custom text for add to cart button.
+     *
+     * @param string $add_to_cart_text The original text.
+     * @param WC_Product $product The WooCommerce product.
+     * @return string The modified text.
+     */
+    public function custom_add_to_cart_text($add_to_cart_text, $product)
+    {
+        if (is_admin()) {
+            return;
+        }
 
-	/**
-	 * Display the lead time message.
-	 **/
-	public function display_lead_time_message()
-	{
-		global $post;
-		if (!is_product() || empty($post)) {
-			return;
-		}
+        if (!(Helper::get_instance())->is_distributor()) {
+            return __('Add to RFQ', 'rct-customization');
+        }
 
-		$product = wc_get_product(get_the_ID());
-		if (!is_a($product, 'WC_Product')) {
-			throw new \Exception(__('Not a WooCommerce product', 'rct-customization'));
-		}
-
-		$lead_time_message = !empty($product->get_meta(LeadTime::PRODUCT_LEAD_TIME_FIELD)) ? $product->get_meta(LeadTime::PRODUCT_LEAD_TIME_FIELD) : get_option(Settings::GLOBAL_LEAD_TIME_FIELD);
-
-		if (!empty($lead_time_message)) {
-			echo '<div class="rct-lead-time-message rct-woocommerce-store-notice">';
-			echo '<p>' . $lead_time_message . '</p>';
-			echo '</div>';
-		}
-	}
-
-	/**
-	 * Add custom text for add to cart button.
-	 * @param  string $add_to_cart_text
-	 * @param  WC_PRoduct $product
-	 * @return string     
-	 */
-	public function custom_add_to_cart_text($add_to_cart_text, $product)
-	{
-		if (is_admin()) {
-			return;
-		}
-
-		if (!(Helper::get_instance())->is_distributor()) {
-			return __('Add to RFQ', 'rct-customization');
-		}
-
-		return $add_to_cart_text;
-	}
+        return $add_to_cart_text;
+    }
 }
--- src/Singleton.php
+++ src/Singleton.php
@@ -11,43 +11,52 @@ defined('ABSPATH') || exit;
 abstract class Singleton
 {
 
-	private static $instances = array();
-	protected function __construct()
-	{
-	}
-	protected function __clone()
-	{
-	}
-	public function __wakeup()
-	{
-		throw new \Exception('Cannot unserialize singleton');
-	}
+    private static $instances = array();
 
+    protected function __construct()
+    {
+    }
 
-	/**
-	 * Get instance object.
-	 **/
-	public static function get_instance()
-	{
-		$cls = get_called_class(); // late-static-bound class name.
-		if (!isset(self::$instances[$cls])) {
-			self::$instances[$cls] = new static;
-			(self::$instances[$cls])->on_construct();
-		}
-		return self::$instances[$cls];
-	}
+    protected function __clone()
+    {
+    }
 
-	function on_construct()
-	{
-	}
+    public function __wakeup()
+    {
+        throw new \Exception('Cannot unserialize singleton');
+    }
 
-	/**
-	 * Returns the singleton instance.
-	 *
-	 * @return $this
-	 */
-	public static function instance()
-	{
-		return static::get_instance();
-	}
+    /**
+     * Get instance object.
+     *
+     * @return static
+     */
+    public static function get_instance()
+    {
+        $cls = get_called_class(); // late-static-bound class name.
+        if (!isset(self::$instances[$cls])) {
+            self::$instances[$cls] = new static;
+            (self::$instances[$cls])->on_construct();
+        }
+        return self::$instances[$cls];
+    }
+
+    /**
+     * Method called on object construction.
+     *
+     * @return void
+     */
+    protected function on_construct()
+    {
+    }
+
+    /**
+     * Returns the singleton instance.
+     *
+     * @return static
+     */
+    public static function instance()
+    {
+        return static::get_instance();
+    }
 }
