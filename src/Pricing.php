e4f2fb332db7823447242b5a1ac0e2ca3407ef32
Muhammad Awais <awais300@gmail.com>
2024-06-01 02:15:45 +0500

    handle RFQ item if sku is missing in cart

--- src/Pricing.php
+++ src/Pricing.php
@@ -3,6 +3,7 @@
 namespace EWA\RCTool;
 
 use EWA\RCTool\Admin\Acf\UserFields;
+use EWA\RCTool\SpecialProductOptions as SpecialProductOptionsFrontend;
 use Exception;
 
 defined('ABSPATH') || exit;
@@ -44,11 +45,143 @@ class Pricing extends Singleton
     public const QTY_PURCHASED = 'qty_purchased';
 
     /**
+     * @var $loader The template loader.
+     **/
+    private $loader = null;
+
+    /**
+     * @var FORM_ID The form ID.
+     **/
+    public const FORM_ID = 4;
+
+    /**
+     * @var IS_PRODUCT_RFQ The key to detect if its RFQ.
+     **/
+    public const IS_PRODUCT_RFQ = '_is_product_rfq';
+
+
+    /**
      * Construct the Pricing class.
      */
     public function __construct()
     {
-        add_action('woocommerce_before_calculate_totals', array($this, 'changing_cart_item_prices'), 20, 1);
+        $this->loader = TemplateLoader::get_instance();
+        add_action('woocommerce_before_calculate_totals', array($this, 'changing_cart_item_prices'), 2002, 1);
+
+        add_filter('woocommerce_cart_item_price', array($this, 'custom_woocommerce_cart_item_price_message'), 5000, 3);
+        add_filter('woocommerce_cart_item_subtotal', array($this, 'custom_woocommerce_cart_item_subtotal_message'), 5000, 3);
+
+        add_action('gform_pre_submission_' . self::FORM_ID, array($this, 'before_form_submit'));
+        add_action('gform_after_submission_' . self::FORM_ID, array($this, 'remove_rfq_products_after_form_submit'), 10);
+    }
+
+    /**
+     * Replace price with custom message.
+     * 
+     * @param $price
+     * @param $cart_item
+     * @param $cart_item_key
+     *
+     * @return string The custom message.
+     **/
+    public function custom_woocommerce_cart_item_price_message($price, $cart_item, $cart_item_key)
+    {
+        if(WC()->session->get(SpecialProductOptionsFrontend::SESS_RC_SPECIAL_PRODUCT) === 'yes') {
+            return $price;
+        }
+
+        $item_price = $cart_item['data']->get_price();
+        $custom_message = $price;
+
+        if ($item_price == 0) {
+            $custom_message = '<a class="rfq-email popmake-19761" href="javascript:void(0);">Email for RFQ</a>';
+        }
+
+        return $custom_message;
+    }
+
+
+    /**
+     * Replace linet item subtotal with custom message.
+     *
+     * @param $subtotal
+     * @param $cart_item
+     * @param $cart_item_key
+     *
+     * @return string The custom message.
+     **/
+    public function custom_woocommerce_cart_item_subtotal_message($subtotal, $cart_item, $cart_item_key)
+    {
+        if(WC()->session->get(SpecialProductOptionsFrontend::SESS_RC_SPECIAL_PRODUCT) === 'yes') {
+            return $subtotal;
+        }
+
+        $item_price = $cart_item['data']->get_price();
+        $custom_message = $subtotal;
+
+        if ($item_price == 0) {
+            $custom_message = '-';
+        }
+
+        return $custom_message;
+    }
+
+    /**
+     * Update cart contents for RFQ before sending email.
+     * This form is for those RFQ proudcts whose SKU is missing in Excel sheet.
+     *
+     * @return void
+     */
+    public function before_form_submit($form)
+    {
+        $keys = $this->get_item_keys_for_missing_skus();
+        $data = array(
+            'keys' => $keys,
+        );
+        $cart_contents = $this->loader->get_template(
+            'cart-contents-missing-sku.php',
+            $data,
+            RCT_CUST_PLUGIN_DIR_PATH . '/templates/',
+            false
+        );
+
+        $_POST['input_8'] = $cart_contents;
+    }
+
+    /**
+     * After sucessfull RFQ email. Remove RFQ products so user can checkout normally.
+     *
+     * @return void
+     */
+    public function remove_rfq_products_after_form_submit($form)
+    {
+        $cart_item_keys = array();
+        $cart = WC()->cart;
+        foreach ($cart->get_cart() as $cart_item_key => $cart_item) {
+            if (isset($cart_item[self::IS_PRODUCT_RFQ]) && $cart_item[self::IS_PRODUCT_RFQ] == 'rfq_yes') {
+                $cart->remove_cart_item($cart_item_key);
+            }
+        }
+
+        return $cart_item_keys;
+    }
+
+    /**
+     * Store current cart item keys in an array.
+     *
+     * @return array The cart time keys.
+     **/
+    public function get_item_keys_for_missing_skus()
+    {
+        $cart_item_keys = array();
+        $cart = WC()->cart;
+        foreach ($cart->get_cart() as $cart_item_key => $cart_item) {
+            if (isset($cart_item[self::IS_PRODUCT_RFQ]) && $cart_item[self::IS_PRODUCT_RFQ] == 'rfq_no') {
+                $cart_item_keys[] = $cart_item_key;
+            }
+        }
+
+        return $cart_item_keys;
     }
 
     /**
@@ -72,13 +205,19 @@ class Pricing extends Singleton
             return;
         }
 
+        $is_rfq_item_exist = false;
+
         // Loop through cart items
-        foreach ($cart->get_cart() as $cart_item) {
+        foreach ($cart->get_cart() as $cart_item_key => $cart_item) {
             $price_level = null;
             $item_sku = $this->get_pc_sku($cart_item);
-            //$item_sku = 'JU963624.UGH';
-            $series_type = $this->detect_series_by_sku($item_sku);
 
+            // For testing purpose only.
+            /*if ($product_id == '19197') {
+                $item_sku = 'JU963624.UGH';
+            }*/
+
+            $series_type = $this->detect_series_by_sku($item_sku);
             if ($item_sku && $series_type) {
                 if ($series_type === self::HISTORICAL) {
                     $price_level = $this->get_historical_pricing_level();
@@ -92,7 +231,25 @@ class Pricing extends Singleton
             $row = $this->get_pricing_levels_by_sku($item_sku);
             if (isset($row->{$price_level})) {
                 $cart_item['data']->set_price($row->{$price_level});
+
+                $cart_content = WC()->cart->cart_contents;
+                $cart_content[$cart_item_key][self::IS_PRODUCT_RFQ] = 'rfq_no';
+                WC()->cart->set_cart_contents($cart_content);
+            } else {
+                $cart_item['data']->set_price(0);
+                $is_rfq_item_exist = true;
+
+                $cart_content = WC()->cart->cart_contents;
+                $cart_content[$cart_item_key][self::IS_PRODUCT_RFQ] = 'rfq_yes';
+                WC()->cart->set_cart_contents($cart_content);
             }
+        }
+
+        if ($is_rfq_item_exist && is_checkout()) {
+            //$redirect_url = add_query_arg(array('rfq-message' => urlencode($custom_message)), wc_get_cart_url());
+            wc_add_notice('Please click on "Email for RFQ" to send the email before proceeding to checkout.', 'notice');
+            wp_redirect(wc_get_cart_url());
+            exit;
         }
     }
 
