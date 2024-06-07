1762c8d58c7e50c42a8c485e6a0af5ee354bf9f7
Muhammad Awais <awais300@gmail.com>
2024-06-07 06:39:53 +0500

    Update cart-contents-missing-sku.php

--- templates/cart-contents-missing-sku.php
+++ templates/cart-contents-missing-sku.php
@@ -1,3 +1,7 @@
+<?php
+
+use EWA\RCTool\Pricing;
+?>
 <table class="shop_table shop_table_responsive cart woocommerce-cart-form__contents" cellspacing="0">
 	<thead>
 		<tr>
@@ -10,27 +14,29 @@
 	</thead>
 	<tbody>
 
-
 		<?php
 		foreach (WC()->cart->get_cart() as $cart_item_key => $cart_item) {
-			if(in_array($cart_item_key, $keys, true)){
-				continue;
-			}
 			$_product   = apply_filters('woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key);
 			$product_id = apply_filters('woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key);
+			/**
+			 * Filter the product name.
+			 *
+			 * @since 2.1.0
+			 * @param string $product_name Name of the product in the cart.
+			 * @param array $cart_item The product in the cart.
+			 * @param string $cart_item_key Key for the product in the cart.
+			 */
+			$product_name = apply_filters('woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key);
 
 			if ($_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters('woocommerce_cart_item_visible', true, $cart_item, $cart_item_key)) {
 				$product_permalink = apply_filters('woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink($cart_item) : '', $cart_item, $cart_item_key);
 		?>
 				<tr class="woocommerce-cart-form__cart-item <?php echo esc_attr(apply_filters('woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key)); ?>">
 
+
 					<td class="product-thumbnail">
 						<?php
 						$thumbnail = apply_filters('woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key);
-
-						$thumbnail = str_ireplace('<img', '<img style="width:50px; height:50px;"', $thumbnail);
-
-						//dd($thumbnail);
 
 						if (!$product_permalink) {
 							echo $thumbnail; // PHPCS: XSS ok.
@@ -43,40 +49,35 @@
 					<td class="product-name" data-title="<?php esc_attr_e('Product', 'woocommerce'); ?>">
 						<?php
 						if (!$product_permalink) {
-							echo wp_kses_post(apply_filters('woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key) . '&nbsp;');
+							echo wp_kses_post($product_name . '&nbsp;');
 						} else {
+							/**
+							 * This filter is documented above.
+							 *
+							 * @since 2.1.0
+							 */
 							echo wp_kses_post(apply_filters('woocommerce_cart_item_name', sprintf('<a href="%s">%s</a>', esc_url($product_permalink), $_product->get_name()), $cart_item, $cart_item_key));
 						}
 
+						do_action('woocommerce_after_cart_item_name', $cart_item, $cart_item_key);
+
 						// Meta data.
-						$formatted_data = wc_get_formatted_cart_item_data($cart_item); // PHPCS: XSS ok.
+						echo wc_get_formatted_cart_item_data($cart_item); // PHPCS: XSS ok.
 
-						/*$style_display_none = ' style="display:none !important;" ';
-						$search = array(
-							'class="mkl_pc-extra-price"',
-							'class="mkl-pc-edit-link--container"',
-							'<img',
-						);
-						$replace = array(
-							'class="mkl_pc-extra-price"' . $style_display_none,
-							'class="mkl-pc-edit-link--container"' . $style_display_none,
-							'<img style="width:25px; height:25px;"',
-						);
-						$formatted_data = str_ireplace($search, $replace, $formatted_data);*/
-
-						$formatted_data = preg_replace('#<span class="mkl_pc-extra-price"?\b(?:(?R)|(?:(?!<\/?div).))*<\/span>#', '', $formatted_data);
-
-						$formatted_data = preg_replace('#<div class="mkl-pc-edit-link--container"?\b(?:(?R)|(?:(?!<\/?div).))*<\/div>#', '', $formatted_data);
-						echo $formatted_data;
-
+						// Backorder notification.
+						if ($_product->backorders_require_notification() && $_product->is_on_backorder($cart_item['quantity'])) {
+							echo wp_kses_post(apply_filters('woocommerce_cart_item_backorder_notification', '<p class="backorder_notification">' . esc_html__('Available on backorder', 'woocommerce') . '</p>', $product_id));
+						}
 						?>
 					</td>
 
 					<td class="product-price" data-title="<?php esc_attr_e('Price', 'woocommerce'); ?>">
 						<?php
-						//echo apply_filters('woocommerce_cart_item_price', WC()->cart->get_product_price($_product), $cart_item, $cart_item_key); // PHPCS: XSS ok.
-						
-						echo '-';
+						if (isset($cart_item[Pricing::IS_PRODUCT_RFQ]) && $cart_item[Pricing::IS_PRODUCT_RFQ] == 'rfq_no' && isset($cart_item[Pricing::PRODUCT_NEW_PRICE])) {
+							echo $cart_item[Pricing::PRODUCT_NEW_PRICE];
+						} else {
+							echo "-";
+						}
 						?>
 					</td>
 
@@ -89,9 +90,11 @@
 
 					<td class="product-subtotal" data-title="<?php esc_attr_e('Subtotal', 'woocommerce'); ?>">
 						<?php
-						//echo apply_filters('woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal($_product, $cart_item['quantity']), $cart_item, $cart_item_key); // PHPCS: XSS ok.
-						
-						echo '-';
+						if (isset($cart_item[Pricing::IS_PRODUCT_RFQ]) && $cart_item[Pricing::IS_PRODUCT_RFQ] == 'rfq_no' && isset($cart_item[Pricing::PRODUCT_NEW_PRICE])) {
+							echo $cart_item['line_subtotal'];
+						} else {
+							echo "-";
+						}
 						?>
 					</td>
 				</tr>
@@ -101,3 +104,4 @@
 		?>
 	</tbody>
 </table>
+</form>
